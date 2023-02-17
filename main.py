f6c5acb3c10fde454565129c86429a8c95d1937b
lrlunin <lunin@campus.tu-berlin.de>
2023-02-17 14:48:38 +0100

    wrong formatted code

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def ddd_dasd(    ):
+    a_very_long_string_which_have_to_be_formatted = np.reshape(np.random.randint(2100, 400*400), (400,400), dtype=np.uint64)
+
+msg1= "different distance"
+msg2='different quoutes'
