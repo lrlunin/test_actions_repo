5fb6c24e8f135c049574e49d960d653dac41c407
lrlunin <lunin.leonid@gmail.com>
2023-02-17 23:20:09 +0100

    minor update

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 
-def ddd_dasd(    ):
-    a_very_long_string_which_have_to_be_formatted = np.reshape(np.random.randint(2100, 400*400), (400,400), dtype=np.uint64)
+def ddd_dasd():
+    a_very_long_string_which_have_to_be_formatted = np.reshape(
+        np.random.randint(2100, 400 * 400), (400, 400), dtype=np.uint64
+    )
 
-msg1= "different distance"
-msg2='different quoutes'
+
+msg1 = "different distance"
+msg2 = "different quoutes"
+
+print("some small update with old version creates no new tag")
