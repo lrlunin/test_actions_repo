bf7e89011a0ebeeade38d8e841e0e5e459ccda88
lrlunin <lunin.leonid@gmail.com>
2023-02-18 18:50:42 +0100

    working conad packaging

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,1 +1,5 @@
-$PYTHON setup.py install     # Python command to install the script.
+cp $RECIPE_DIR/ArgsPrinterTemplate $PREFIX/bin/ArgsPrinter
+echo -e "$PYTHON args_printer.py \$*" >> $PREFIX/bin/ArgsPrinter
+# here creating a symlink
+# ln -s $RECIPE_DIR/main.py $PREFIX/bin/Main
+# what to do with #!/bin/python/ ???
