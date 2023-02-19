afab92201891b30331aafb6edd78fe7c049b88d4
lrlunin <lunin.leonid@gmail.com>
2023-02-19 14:11:30 +0100

    linux packaging

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,5 +1,6 @@
 cp $RECIPE_DIR/ArgsPrinterTemplate $PREFIX/bin/ArgsPrinter
 echo -e "$PYTHON args_printer.py \$*" >> $PREFIX/bin/ArgsPrinter
+echo "Do not forget to provide config files!"
 # here creating a symlink
 # ln -s $RECIPE_DIR/main.py $PREFIX/bin/Main
 # what to do with #!/bin/python/ ???
--- args_printer.py
+++ args_printer.py
@@ -1,3 +1,4 @@
 import sys
+import tango
 for arg in sys.argv[1:]:
     print(arg)
