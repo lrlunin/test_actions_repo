8517561e5dad22358373744d3c69e3eb9171a6f8
lrlunin <lunin.leonid@gmail.com>
2023-02-19 17:07:34 +0100

    fixed linux-build

--- .conda/build.sh
+++ .conda/build.sh
@@ -2,7 +2,7 @@ touch $PREFIX/bin/ArgsPrinter
 cp -R $SRC_DIR/src $SP_DIR/$PKG_NAME
 chmod +x $PREFIX/bin/ArgsPrinter
 echo "#!/bin/bash" > $PREFIX/bin/ArgsPrinter
-echo -e "$PYTHON args_printer.py \$*" >> $PREFIX/bin/ArgsPrinter
+echo -e "$PYTHON $SP_DIR/$PKG_NAME/args_printer.py \$*" >> $PREFIX/bin/ArgsPrinter
 # here creating a symlink
 # ln -s $RECIPE_DIR/main.py $PREFIX/bin/Main
 # what to do with #!/bin/python/ ???
